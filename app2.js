226971d9aec9ed13efeb857f0ec70f5a55471d9c
felipe-frazao <felipe.frazao@al.infnet.edu.br>
2017-10-19 19:30:02 -0200

    adicionado classe pessoas, metodos para retornar resultados.

--- app2.js
+++ app2.js
@@ -34,3 +34,28 @@ var Concessionaria = (function () {
     ;
     return Concessionaria;
 }());
+var Pessoa = (function () {
+    function Pessoa(nome, carroPreferido) {
+        this.nome = nome;
+        this.carroPreferido = carroPreferido;
+    }
+    Pessoa.prototype.dizerNome = function () {
+        return this.nome;
+    };
+    ;
+    Pessoa.prototype.dizerCarroPreferido = function () {
+        return this.carroPreferido;
+    };
+    ;
+    Pessoa.prototype.comprarCarro = function (carro) {
+        this.carro = carro;
+    };
+    ;
+    Pessoa.prototype.dizerCarroQueTem = function () {
+        return this.carro;
+    };
+    ;
+    return Pessoa;
+}());
+var pessoa = new Pessoa('José ', "Chevet");
+console.log(pessoa.dizerCarroPreferido());
--- app2.ts
+++ app2.ts
@@ -33,3 +33,29 @@ class Concessionaria {
         return this.listaDeCarros;
     };
 }
+class Pessoa {
+    private nome: string;
+    private carroPreferido: string;
+    private carro: string;
+
+    constructor(nome: string, carroPreferido: string) {
+        this.nome = nome;
+        this.carroPreferido = carroPreferido;
+    }
+
+    public dizerNome(): string {
+        return this.nome;
+    };
+    public dizerCarroPreferido(): string {
+        return this.carroPreferido;
+    };
+    public comprarCarro(carro: any): void {
+        this.carro = carro;
+    };
+    public dizerCarroQueTem(): any {
+        return this.carro;
+    };
+}
+
+let pessoa = new Pessoa('José ', "Chevet");
+console.log(pessoa.dizerCarroPreferido());
